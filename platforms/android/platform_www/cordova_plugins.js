9c260e9ec5be568f6c216537d97432aff959bb91
Pablo <pablo.hernan.arg@gmail.com>
2016-02-27 16:22:28 -0300

    Signed-off-by: Pablo <pablo.hernan.arg@gmail.com>

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,20 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/plugin.google.maps/www/googlemaps-cdv-plugin.js",
+        "id": "plugin.google.maps.phonegap-googlemaps-plugin",
+        "clobbers": [
+            "plugin.google.maps"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{}
+// BOTTOM OF METADATA
+});
